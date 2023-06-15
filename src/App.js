e08524f1a071ea04aee953971737a39a01198c92
bishowshr1 <shrestharahul223@gmail.com>
2023-06-15 11:37:44 +0545

    changes

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const App = () => {
+
+
+
+
+
+  const data = [
+
+    {
+      "strMeal": "Baked salmon with fennel & tomatoes",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+      "idMeal": "52959"
+    },
+    {
+      "strMeal": "Cajun spiced fish tacos",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+      "idMeal": "52819"
+    },
+    {
+      "strMeal": "Escovitch Fish",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+      "idMeal": "52944"
+    },
+    {
+      "strMeal": "Fish fofos",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
+      "idMeal": "53043"
+    },
+    {
+      "strMeal": "Fish pie",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg",
+      "idMeal": "52802"
+    },
+    {
+      "strMeal": "Fish Stew with Rouille",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/vptqpw1511798500.jpg",
+      "idMeal": "52918"
+    },
+    {
+      "strMeal": "Garides Saganaki",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/wuvryu1468232995.jpg",
+      "idMeal": "52764"
+    },
+    {
+      "strMeal": "Grilled Portuguese sardines",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/lpd4wy1614347943.jpg",
+      "idMeal": "53041"
+    },
+    {
+      "strMeal": "Honey Teriyaki Salmon",
+      "strMealThumb": "https://www.themealdb.com/images/media/meals/xxyupu1468262513.jpg",
+      "idMeal": "52773"
+    },
+  ];
+
+
+
+
+  return (
+    <div className='p-5 grid grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4'>
+
+      {data.map((food, i) => {
+
+        return <div key={i} className='shadow-lg'>
+          <h1>{food.strMeal}</h1>
+          <img src={food.strMealThumb} alt="" />
+          <p>{food.idMeal}</p>
+
+        </div>
+
+
+      })}
+
+
+
+    </div >
+  )
+}
+
+export default App
